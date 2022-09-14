8e7c41629af5baa8dfe03a60ca9fd93d64950d31
antonepifanov <antonepifanov87@gmail.com>
2022-09-14 21:04:52 +0300

    Отрисовывает компоненты на странице согласно задания

--- src/view/trip-coast.js
+++ src/view/trip-coast.js
@@ -1,5 +1,5 @@
 export const createTripCoastTemplate = () => (
-   `<p class="trip-info__cost">
+  `<p class="trip-info__cost">
       Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
    </p>`
 );
