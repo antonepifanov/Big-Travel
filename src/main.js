ca431b199b3140d0c4049a3ef3754275e9cd43a5
antonepifanov <antonepifanov87@gmail.com>
2022-10-07 23:23:15 +0300

    Добавляет логику отображения в компонент фильтров

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ import {getRandomInteger} from './utilities.js';
 import {createTripInfoTemplate} from './view/trip-info.js';
 import {createMenuTemplate} from './view/menu.js';
 import {createTripCoastTemplate} from './view/trip-coast.js';
-import {createFiltersTemplate} from './view/trip-filters.js';
+import {createFilterTemplate} from './view/trip-filters.js';
 import {createSortingTemplate} from './view/sorting.js';
 import {createPointsListTemplate} from './view/points-list.js';
 import {createEditPointTemplate} from './view/edit-point.js';
 import {createPointTemplate} from './view/point.js';
+import {generateFilters} from './mock/generate-filters.js';
 import {MOCK_EVENTS} from './mock/constants.js';
 import {generatePoint} from './mock/generate-point.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const mockPoints = Array.from({length: getRandomInteger(MOCK_EVENTS.MIN, MOCK_EVENTS.MAX)}, () => generatePoint());
+const filters = generateFilters(mockPoints);
 
 const tripMain = document.querySelector('.trip-main');
 render(tripMain, createTripInfoTemplate(), 'afterbegin');
@@ -24,7 +28,7 @@ const pageNav = tripMain.querySelector('.trip-controls__navigation');
 render(pageNav, createMenuTemplate(), 'beforeend');
 
 const tripFilters = tripMain.querySelector('.trip-controls__filters');
-render(tripFilters, createFiltersTemplate(), 'beforeend');
+render(tripFilters, createFilterTemplate(filters), 'beforeend');
 
 const mainContent = document.querySelector('.trip-events');
 render(mainContent, createSortingTemplate(), 'beforeend');
@@ -33,10 +37,8 @@ render(mainContent, createPointsListTemplate(), 'beforeend');
 
 const pointsList = mainContent.querySelector('.trip-events__list');
 
-const mockData = Array.from({length: getRandomInteger(MOCK_EVENTS.MIN, MOCK_EVENTS.MAX)}, () => generatePoint());
+render(pointsList, createEditPointTemplate(mockPoints[0]), 'afterbegin');
 
-render(pointsList, createEditPointTemplate(), 'afterbegin');
-
-mockData.slice(1).forEach((mockEvent) => {
-  render(pointsList, createPointTemplate(mockEvent), 'beforeend');
+mockPoints.slice(1).forEach((mockPoint) => {
+  render(pointsList, createPointTemplate(mockPoint), 'beforeend');
 });
--- src/view/trip-filters.js
+++ src/view/trip-filters.js
@@ -1,20 +1,13 @@
-export const createFiltersTemplate = () => (
-  `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
+export const createFilterTemplate = (filterItems) => {
+  const filterItemsTemplate = filterItems.map((filter, index) => (
+    `<div class="trip-filters__filter">
+      <input id="filter-${filter.name.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.name.toLowerCase()}" ${index === 0 ? 'checked' : ''}>
+      <label class="trip-filters__filter-label" for="filter-${filter.name.toLowerCase()}">${filter.name}</label>
+    </div>`
+  )).join('');
 
-      <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-      <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
-
-      <button class="visually-hidden" type="submit">Accept filter</button>
-   </form>`
-);
+  return `<form class="trip-filters" action="#" method="get">
+            ${filterItemsTemplate}
+            <button class="visually-hidden" type="submit">Accept filter</button>
+          </form>`;
+};
